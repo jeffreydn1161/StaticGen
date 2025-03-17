6c0f5ea0256ea0c300ecbaa5f7ece34ebfeb0bd2
jeffreydn1161 <jeffreydn1161@gmail.com>
2025-03-17 14:31:06 -0400

    some progress

---
+++ src/test_leafnode.py
@@ -0,0 +1,13 @@
+import unittest
+from htmlnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+if __name__ == "__main__":
+    unittest.main()
